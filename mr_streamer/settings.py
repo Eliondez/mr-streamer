6a523c24a39b08a3eb3700dfdc364dfe7cd7b38d
IvanF <dz_elf@mail.ru>
2021-08-07 23:55:21 +0300

    added react part

--- mr_streamer/settings.py
+++ mr_streamer/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'mr_streamer.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,6 +126,13 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- mr_streamer/urls.py
+++ mr_streamer/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import index_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index', index_view),
     path('api/market-data/', include('marketdata.urls'))
 ]
